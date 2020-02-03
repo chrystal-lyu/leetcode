2c7d483b02d92685101fa8dc5d598f7817124c78
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-02-02 17:31:12 -0800

    solved 455

--- 0455_easy_assign_cookie.js
+++ 0455_easy_assign_cookie.js
@@ -2,7 +2,7 @@ var findContentChildren = function(g, s) {
   g = g.sort((a,b) => { return a - b});
   s = s.sort((a,b) => { return a - b});
   let i = 0;
-  for (j=0; i<g.length, j<s.length; j++) {
+  for (let j=0; i<g.length, j<s.length; j++) {
     if (s[j] >= g[i]) {
       i++;
     }
